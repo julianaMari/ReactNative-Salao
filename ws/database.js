af0d9b7b1ff1db3a17b796250ce189eee22f34e3
Juliana Cruz <julianamariacruz.jm@gmail.com>
2025-08-05 10:41:33 -0300

    iniciando rotas e services e criando arquivo .env e gitignore

--- ws/database.js
+++ ws/database.js
@@ -1,13 +1,25 @@
 const mongoose = require('mongoose');
-const API_KEY = process.env.DATABASE_URL;
-const URI = API_KEY;
+const URI = process.env.DATABASE_URL;
+
+mongoose
+.connect(URI)
+.then(() => console.log('DB is Up!'))
+.catch((err) => console.log(err)); // Adicione 'err' ao parâmetro para ver o erro no console
+
+
+
+
+
+//const mongoose = require('mongoose');
+//const API_KEY = process.env.DATABASE_URL;
+//const URI = API_KEY;
 
 // mongoose.set('useNewUrlParser', true);
 // mongoose.set('useFindAndModify', false);
 // mongoose.set('useCreateIndex', true);
 // mongoose.set('useUnifiedTopology', true);
 
-mongoose
-.connect(URI)
-.then(() => console.log('DB is Up!'))
-.catch(() => console.log(err));
+//mongoose
+//.connect(URI)
+//.then(() => console.log('DB is Up!'))
+//.catch(() => console.log(err));
